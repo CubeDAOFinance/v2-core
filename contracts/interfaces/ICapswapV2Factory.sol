722238a0737904dc982d79bbaf6cd3e1c00dbb2e
capricornswap <capricornswap@gmail.com>
2022-04-01 15:44:36 +0800

    uni -> cap

--- contracts/interfaces/ICapswapV2Factory.sol
+++ contracts/interfaces/ICapswapV2Factory.sol
@@ -1,9 +1,12 @@
 pragma solidity >=0.5.0;
 
-interface IUniswapV2Factory {
+interface ICapswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
+    function initCodeHash() external view returns (bytes32);
+
     function feeTo() external view returns (address);
+    function feeToRate() external view returns (uint numerator,uint denominator);
     function feeToSetter() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
@@ -13,5 +16,6 @@ interface IUniswapV2Factory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
+    function setFeeToRate(uint numerator,uint denominator) external;
     function setFeeToSetter(address) external;
 }
