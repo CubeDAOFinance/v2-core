5094730462eda2b82aeff63611b24372a6cf3f4b
CUBEDAO <108047629+CubeDAOFinance@users.noreply.github.com>
2022-06-26 21:07:41 +0600

    Update and rename CapswapV2Factory.sol to CubeswapV2Factory.sol

--- contracts/CubeswapV2Factory.sol
+++ contracts/CubeswapV2Factory.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
-import './interfaces/ICapswapV2Factory.sol';
-import './CapswapV2Pair.sol';
+import './interfaces/ICubeswapV2Factory.sol';
+import './CubeswapV2Pair.sol';
 
-contract CapswapV2Factory is ICapswapV2Factory {
+contract CubeswapV2Factory is ICubeswapV2Factory {
     address public feeTo;
     address public feeToSetter;
     bytes32 public initCodeHash;
@@ -18,7 +18,7 @@ contract CapswapV2Factory is ICapswapV2Factory {
 
     constructor(address _feeToSetter) public {
         feeToSetter = _feeToSetter;
-        initCodeHash = keccak256(type(CapswapV2Pair).creationCode);
+        initCodeHash = keccak256(type(CubeswapV2Pair).creationCode);
     }
 
     function allPairsLength() external view returns (uint) {
@@ -26,16 +26,16 @@ contract CapswapV2Factory is ICapswapV2Factory {
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'CapswapV2: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, 'CubeswapV2: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'CapswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'CapswapV2: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(CapswapV2Pair).creationCode;
+        require(token0 != address(0), 'CubeswapV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'CubeswapV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(CubeswapV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        ICapswapV2Pair(pair).initialize(token0, token1);
+        ICubeswapV2Pair(pair).initialize(token0, token1);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
@@ -48,17 +48,17 @@ contract CapswapV2Factory is ICapswapV2Factory {
     }
 
     function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'CapswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'CubeswapV2: FORBIDDEN');
         feeTo = _feeTo;
     }
 
     function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'CapswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'CubeswapV2: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 
     function setFeeToRate(uint numerator,uint denominator) external{
-        require(msg.sender == feeToSetter, 'CapswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'CubeswapV2: FORBIDDEN');
         require(denominator >= numerator);
         feeToNumerator = numerator;
         feeToDenominator = denominator;
