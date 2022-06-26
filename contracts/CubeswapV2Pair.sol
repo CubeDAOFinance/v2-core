7e2fae2ee9a5d9bf44a15ae4eb4c9cfdb517a31d
CUBEDAO <108047629+CubeDAOFinance@users.noreply.github.com>
2022-06-26 21:02:21 +0600

    Update and rename CapswapV2Pair.sol to CubeswapV2Pair.sol

--- contracts/CubeswapV2Pair.sol
+++ contracts/CubeswapV2Pair.sol
@@ -1,14 +1,14 @@
 pragma solidity =0.5.16;
 
-import './interfaces/ICapswapV2Pair.sol';
-import './CapswapV2ERC20.sol';
+import './interfaces/ICubeswapV2Pair.sol';
+import './CubeswapV2ERC20.sol';
 import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IERC20.sol';
-import './interfaces/ICapswapV2Factory.sol';
-import './interfaces/ICapswapV2Callee.sol';
+import './interfaces/ICubeswapV2Factory.sol';
+import './interfaces/ICubeswapV2Callee.sol';
 
-contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
+contract CubeswapV2Pair is ICubeswapV2Pair, CubeswapV2ERC20 {
     using SafeMath  for uint;
     using UQ112x112 for uint224;
 
@@ -29,7 +29,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
 
     uint private unlocked = 1;
     modifier lock() {
-        require(unlocked == 1, 'CapswapV2: LOCKED');
+        require(unlocked == 1, 'CubeswapV2: LOCKED');
         unlocked = 0;
         _;
         unlocked = 1;
@@ -43,7 +43,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
 
     function _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'CapswapV2: TRANSFER_FAILED');
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'CubeswapV2: TRANSFER_FAILED');
     }
 
     event Mint(address indexed sender, uint amount0, uint amount1);
@@ -64,10 +64,10 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
 
     // called once by the factory at time of deployment
     function initialize(address _token0, address _token1) external {
-        require(msg.sender == factory, 'CapswapV2: FORBIDDEN'); // sufficient check
+        require(msg.sender == factory, 'CubeswapV2: FORBIDDEN'); // sufficient check
         token0 = _token0;
         token1 = _token1;
-        name = string(abi.encodePacked('Capricorn Pair ',getTokenName(token0),' - ',getTokenName(token1)));
+        name = string(abi.encodePacked('Cubedao Pair ',getTokenName(token0),' - ',getTokenName(token1)));
         symbol= string(abi.encodePacked(getTokenSymbol(token0),'-',getTokenSymbol(token1)));
     }
 
@@ -101,7 +101,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
 
     // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
-        address feeTo = ICapswapV2Factory(factory).feeTo();
+        address feeTo = ICubeswapV2Factory(factory).feeTo();
         feeOn = feeTo != address(0);
         uint _kLast = kLast; // gas savings
         if (feeOn) {
@@ -109,7 +109,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
                 uint rootK = Math.sqrt(uint(_reserve0).mul(_reserve1));
                 uint rootKLast = Math.sqrt(_kLast);
                 if (rootK > rootKLast) {
-                    (uint n,uint d) = ICapswapV2Factory(factory).feeToRate();
+                    (uint n,uint d) = ICubeswapV2Factory(factory).feeToRate();
                     if(d == 0){
                         d = 1;
                     }
@@ -140,7 +140,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
         } else {
             liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
         }
-        require(liquidity > 0, 'CapswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        require(liquidity > 0, 'CubeswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
 
         _update(balance0, balance1, _reserve0, _reserve1);
@@ -161,7 +161,7 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
-        require(amount0 > 0 && amount1 > 0, 'CapswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        require(amount0 > 0 && amount1 > 0, 'CubeswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
         _burn(address(this), liquidity);
         _safeTransfer(_token0, to, amount0);
         _safeTransfer(_token1, to, amount1);
@@ -175,29 +175,29 @@ contract CapswapV2Pair is ICapswapV2Pair, CapswapV2ERC20 {
 
     // this low-level function should be called from a contract which performs important safety checks
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
-        require(amount0Out > 0 || amount1Out > 0, 'CapswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(amount0Out > 0 || amount1Out > 0, 'CubeswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
-        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'CapswapV2: INSUFFICIENT_LIQUIDITY');
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'CubeswapV2: INSUFFICIENT_LIQUIDITY');
 
         uint balance0;
         uint balance1;
         { // scope for _token{0,1}, avoids stack too deep errors
         address _token0 = token0;
         address _token1 = token1;
-        require(to != _token0 && to != _token1, 'CapswapV2: INVALID_TO');
+        require(to != _token0 && to != _token1, 'CubeswapV2: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) ICapswapV2Callee(to).capswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) ICubeswapV2Callee(to).cubeswapV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'CapswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        require(amount0In > 0 || amount1In > 0, 'CubeswapV2: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
         uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
         uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'CapswapV2: K');
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'CubeswapV2: K');
         }
 
         _update(balance0, balance1, _reserve0, _reserve1);
